ad384d7024912e786cedb179fbb03101829a23f8
Rick South <rick@natamus.com>
2023-12-12 00:33:19 +0100

    Pet Names source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/petnames/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.petnames;
+
+import com.natamus.petnames.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
