85788ca6daf77679a6da61346c9303aa420b8321
Rick South <rick@natamus.com>
2024-08-09 08:11:59 +0200

    Pet Names source code for 1.21.1.

---
+++ Fabric/src/main/java/com/natamus/petnames/ModFabric.java
@@ -0,0 +1,39 @@
+package com.natamus.petnames;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveAnimalEvents;
+import com.natamus.petnames.events.NamingEvent;
+import com.natamus.petnames.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.animal.Animal;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveAnimalEvents.PRE_BABY_SPAWN.register((ServerLevel world, Animal parentA, Animal parentB, AgeableMob offspring) -> {
+			NamingEvent.onBaby(world, parentA, parentB, offspring);
+			return true;
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
