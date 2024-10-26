498cd30bb35c33f7bd9079e46bb4e0157a48e97b
Rick South <rick@natamus.com>
2024-10-26 15:05:34 +0200

    Pet Names source code for 1.21.3.

---
+++ Forge/src/main/java/com/natamus/petnames/forge/events/ForgeNamingEvent.java
@@ -0,0 +1,31 @@
+package com.natamus.petnames.forge.events;
+
+import com.natamus.petnames.events.NamingEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeNamingEvent {
+	@SubscribeEvent
+	public void onBaby(BabyEntitySpawnEvent e) {
+		Entity entity = e.getChild();
+		Level level = entity.level();
+		if (level.isClientSide) {
+			return;
+		}
+
+		Mob parentA = e.getParentA();
+		Mob parentB = e.getParentB();
+		if (!(parentA instanceof Animal) || !(parentB instanceof Animal)) {
+			return;
+		}
+
+		NamingEvent.onBaby((ServerLevel)level, (Animal)parentA, (Animal)parentB, e.getChild());
+	}
+}
